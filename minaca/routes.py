fed59c024d63c3c5505e827696859a631a7e22b1
Jiho Lee <search5@gmail.com>
2021-12-02 21:26:24 +0900

    API 경로 변경

--- minaca/routes.py
+++ minaca/routes.py
@@ -1,5 +1,6 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('api_test', '/api/gyeongsil')
     config.add_route('home', '/{route_path:.*}')
     config.add_route('catchall_static_js', '/js/*subpath')
     config.add_route('catchall_static_css', '/css/*subpath')
--- minaca/views/default.py
+++ minaca/views/default.py
@@ -10,3 +10,10 @@ from .. import models
 @view_config(route_name='home', renderer='minaca:templates/index.jinja2')
 def my_view(request):
     return {}
+
+
+@view_config(route_name='api_test', renderer='json')
+def api_test(request):
+    return {
+        'dest': 'lee gyeong sil'
+    }
